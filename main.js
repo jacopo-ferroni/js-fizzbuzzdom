75e55fbc776923e3a9e9b817f67eedfe777dcd9b
jacopo-ferroni <jacopoferroni2706@gmail.com>
2021-10-25 15:43:31 +0200

    end

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 const container = document.querySelector(`.container`);
 
-for(let i = 0; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
     if ((i % 3 == 0) && !(i % 5 == 0)) {
-    container.innerHTML = (`<div class="box1 color0">fizz</div>`);
-    }
-    else if ((i % 5 == 0) && !(i % 3 == 0)) {
-        container.innerHTML = (`<div class="box1 color1">buzz</div>`);
-    }
-    else if ((i % 3 == 0) && (i % 5 == 0)) {
-        container.innerHTML = (`<div class="box1 color2">fizzbuzz</div>`);
-    }
-    else {
-        container.innerHTML = (`<div class="box1 color3">${i}</div>`);
-    }
+        container.innerHTML += `<div class="box1 color0">fizz</div>`;
+        }
+        else if ((i % 5 == 0) && !(i % 3 == 0)) {
+            container.innerHTML += `<div class="box1 color1">buzz</div>`;
+        }
+        else if ((i % 3 == 0) && (i % 5 == 0)) {
+            container.innerHTML += `<div class="box1 color2">fizzbuzz</div>`;
+        }
+        else {
+            container.innerHTML += `<div class="box1 color3">${i}</div>`;
+        }
 }
